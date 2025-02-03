bdd86ecff4e223888e03bcf862363c793c6e3e43
Supun Wimalasena <44988673+supun-io@users.noreply.github.com>
2025-02-03 11:30:12 +0100

    Post (#5)
    * post added
    
    * init for post

--- meta/compose/pgsql/initdb.d/init.sh
+++ meta/compose/pgsql/initdb.d/init.sh
@@ -15,7 +15,7 @@ function create_user_and_database() {
 EOSQL
 }
 
-base_dbs="hyvor hyvor_blogs hyvor_talk"
+base_dbs="hyvor hyvor_blogs hyvor_talk hyvor_post"
 
 for db in $base_dbs; do
   echo "Creating DB variants for $db"
