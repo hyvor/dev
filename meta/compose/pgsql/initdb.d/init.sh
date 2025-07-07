9cd63cc4bd8a67d3833365044bac985014c9175a
Supun Wimalasena <supun@hyvor.com>
2025-07-07 20:49:46 +0200

    wip

---
+++ meta/compose/pgsql/initdb.d/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# @source: https://github.com/mrts/docker-postgresql-multiple-databases/blob/master/create-multiple-postgresql-databases.sh
+
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$1
+	echo "  Creating user and database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $database;
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+EOSQL
+}
+
+base_dbs="hyvor hyvor_blogs hyvor_talk hyvor_post"
+
+for db in $base_dbs; do
+  echo "Creating DB variants for $db"
+  create_user_and_database $db
+  create_user_and_database $db"_testing"
+  create_user_and_database $db"_e2e"
+done
